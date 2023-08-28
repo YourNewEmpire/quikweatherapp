75a6674aa409b3f09b93fc27c4ab97ae08da9010
YourNewEmpire <archiesmyth26@googlemail.com>
2023-08-28 12:56:10 +0100

    chore: move repeated screen options to a var

--- App.js
+++ App.js
@@ -1,49 +1,40 @@
-import { NavigationContainer } from "@react-navigation/native";
 import { Text } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
-
 import { store, persistor } from "./redux/store";
 import Home from "./pages/home";
 import SavedScreen from "./pages/saved";
 import styles from "./style";
 const Stack = createNativeStackNavigator();
+const screenOptions = {
+  title: "QuikWether",
+  headerStyle: {
+    backgroundColor: styles.regalGreen,
+  },
+  headerTintColor: "#fff",
+  headerTitleStyle: {
+    fontWeight: "bold",
+  },
+};
 
 export default function App() {
   return (
     <Provider store={store}>
       <PersistGate loading={<Text>Loading...</Text>} persistor={persistor}>
         <NavigationContainer>
-          <Stack.Navigator>
+          <Stack.Navigator initialRouteName="Home">
             <Stack.Screen
               name="Home"
               component={Home}
-              options={{
-                title: "QuikWeather",
-                headerStyle: {
-                  backgroundColor: styles.regalGreen,
-                },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
+              options={screenOptions}
             />
             <Stack.Screen
               name="Saved"
               component={SavedScreen}
-              options={{
-                title: "Saved Locations",
-                headerStyle: {
-                  backgroundColor: styles.regalGreen,
-                },
-                headerTintColor: "#fff",
-                headerTitleStyle: {
-                  fontWeight: "bold",
-                },
-              }}
+              options={screenOptions}
             />
           </Stack.Navigator>
           <StatusBar style="light" />
