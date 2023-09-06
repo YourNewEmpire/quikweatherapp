82b7c697b6d7bdfba21db652d5f44b5ef5a875af
YourNewEmpire <archiesmyth26@googlemail.com>
2023-09-06 17:15:46 +0100

    feat: clean up saved page & use Location component

--- pages/saved/index.js
+++ pages/saved/index.js
@@ -1,48 +1,45 @@
 import { Text, Pressable, ScrollView, View } from "react-native";
-import { remove, clearAll, setCurrent } from "../../redux/reducers/locations";
+import { useState } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { clearAll } from "../../redux/reducers/locations";
 import styles from "../../style";
-import Ionicons from "@expo/vector-icons/Ionicons";
-import { useEffect } from "react";
-
-export default SavedScreen = ({ navigation, route }) => {
-  const dispatch = useDispatch();
-  // const [locations, setLocations] = useState([]);
+import CenteredModal from "../../components/CenteredModal";
+import Location from "../../components/Location";
+export default SavedScreen = ({ navigation }) => {
   const locations = useSelector((state) => state.location.locations);
+  const dispatch = useDispatch();
+  const [clearAllModal, setClearAllModal] = useState(false);
 
+  const handleOpenModal = () => {
+    setClearAllModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setClearAllModal(false);
+  };
+  const handleClear = () => {
+    dispatch(clearAll());
+    setClearAllModal(false);
+  };
   return (
     <ScrollView style={{ backgroundColor: "#000", padding: 8 }}>
+      <CenteredModal
+        visible={clearAllModal}
+        onClose={handleCloseModal}
+        action={handleClear}
+        title="Are you sure you want to clear all locations?"
+        actionText="Confirm & Clear All"
+      />
+
       <Text>This is your saved locations</Text>
       <View style={styles.locList}>
         {locations.map((loc) => (
-          <View key={loc.name} style={styles.locItem}>
-            <Pressable
-              style={{ ...styles.styledButton, flex: 5 }}
-              onPress={() => {
-                dispatch(setCurrent(loc.data));
-                navigation.navigate("Home");
-              }}
-            >
-              <Text style={{ ...styles.text, fontWeight: 700 }}>
-                {loc.name}
-              </Text>
-            </Pressable>
-            <Pressable
-              style={{
-                ...styles.styledButton,
-                flex: 1,
-                justifyContent: "center",
-              }}
-              onPress={() => dispatch(remove(loc))}
-            >
-              <Text style={styles.text}>X</Text>
-            </Pressable>
-          </View>
+          <Location key={loc.name} location={loc} nav={navigation} />
         ))}
       </View>
       <Pressable
         style={{ ...styles.styledButton, marginTop: 32 }}
-        onPress={() => dispatch(clearAll())}
+        onPress={handleOpenModal}
       >
         <Text style={styles.text}>Clear all</Text>
       </Pressable>
